2bad0e466d237d3c1d4a4b1b2d82471fa09f4e69
Marco Dalla Libera <marco@dallalibera.net>
2018-11-29 11:59:44 +0100

    Aggiungere i file di progetto.

---
+++ XamarinFormsMvvm/XamarinFormsMvvm/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsMvvm
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel vm = new MainPageViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = vm;
+        }
+
+        //Senza Command ho bisogno di un hendler nel code-behind
+        private void ButtonClicked(object sender,
+                               EventArgs e)
+        {
+            //WelcomeTextLabel.Text = "Ciao a tutti!";
+            vm.WelcomeText = "Ciao a tutti!";
+        }
+
+    }
+}
---
+++ XamarinFormsMvvm/XamarinFormsMvvm/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinFormsMvvm
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        public ICommand ButtonClickedCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            ButtonClickedCommand = new Command(OnButtonClicked);
+        }
+
+        private string welcomeText = "...";
+        public string WelcomeText
+        {
+            get => welcomeText;
+            set => SetValue(ref welcomeText, value);
+        }
+
+        private void OnButtonClicked()
+        {
+            WelcomeText = "Ciao da un Bound Command!!";
+}
+    }
+
+}
